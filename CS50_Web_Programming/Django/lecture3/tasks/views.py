ebc098b3bd99775c8955265a260ba065cfaee60b
apenfe <adrian1414@hotmail.es>
2024-07-02 16:27:21 +0200

    Tasks y enlaces Django

--- CS50_Web_Programming/Django/lecture3/tasks/views.py
+++ CS50_Web_Programming/Django/lecture3/tasks/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
+from django import forms
 
 # Create your views here.
 tasks = ['foo','bar','baz']
+
+class NewTaskForm(forms.Form):
+    task = forms.CharField(label="New Task")
+    priority = forms.IntegerField(label="Priority",min_value=1,max_value=10)
+
 def index(request):
     return render(request, 'tasks/index.html',{
         'tasks': tasks
     })
 
 def add(request):
-    return render(request, 'tasks/add.html')
+    return render(request, 'tasks/add.html', {
+        "form": NewTaskForm()
+    })
