5eae32868f745cc5a860e0902cc472e2a35b78d2
apenfe <adrian1414@hotmail.es>
2024-07-02 12:30:13 +0200

    Primer HTML template en Django

--- CS50_Web_Programming/Django/lecture3/hello/urls.py
+++ CS50_Web_Programming/Django/lecture3/hello/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('adrian', views.adrian, name='adrian'),
+    path('<str:name>', views.greet, name='greet'),
 ]
--- CS50_Web_Programming/Django/lecture3/hello/views.py
+++ CS50_Web_Programming/Django/lecture3/hello/views.py
@@ -3,4 +3,10 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello World")
+    return render(request, 'hello/index.html')
+
+def adrian(request):
+    return HttpResponse("Hola, Adrián")
+
+def greet(request, name):
+    return HttpResponse(f"Hello, {name.title()}")
